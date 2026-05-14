b1c978ef3d6808c9b158b47c0e0355916ca41598
dustingjava <152653472+dustingjava@users.noreply.github.com>
2026-05-14 14:40:45 -0400

    TwoSum

---
+++ src/TwoSum.java
@@ -0,0 +1,36 @@
+// Two Sum (LeetCode #1)
+// Given an array of integers nums and an integer target, return the indices of the two numbers that add up to target. You may assume exactly one solution exists and you cannot use the same element twice.
+// Example 1:
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: nums[0] + nums[1] = 2 + 7 = 9
+
+// Example 2:
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+
+// Example 3:
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+// Constraints:
+
+// You must write an algorithm better than O(n²) time
+// Try to solve it in O(n) time
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            
+            map.put(nums[i], i);
+        }
+        
+        return new int[]{};
+    }
+}
